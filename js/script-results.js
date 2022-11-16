c2e64befecd36228f5b25237f99cefccd6dbf0ea
dinoiacono <dino.iacono.29@gmail.com>
2022-11-16 17:41:44 +0100

    grafico

--- js/script-results.js
+++ js/script-results.js
@@ -17,3 +17,31 @@ document.querySelector('#errate').innerHTML = percentualeErrate + '%';
 document.querySelector('#contatore-corrette').innerHTML = numeroRisultatoCorr + '/' + domande + ' questions';
 document.querySelector('#contatore-errate').innerHTML = numeroRisultatoErr + '/' + domande + ' questions';
 
+let myCanvas = document.querySelector("#myCanvas").getContext("2d");
+  let myLabels = ["Sbagliate", "Giuste"];
+  let myData =  [numeroRisultatoErr, numeroRisultatoCorr];
+
+  let chart = new Chart(myCanvas, {
+    type: "doughnut",
+    data: {
+      labels: myLabels,
+      datasets: [
+        {
+          label: "Punteggio",
+          data: myData,
+          backgroundColor: ["#D20094", "#00FFFF"],
+          hoverBorderColor: ,
+          hoverBorderWidth: 1,
+          hoverOffset: 2,
+          cutout: "70%",
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+    },
+  });
