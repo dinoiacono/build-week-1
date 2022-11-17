6cb9e53c311e49a0ca83cad53de13525c1b5c0f8
ManuelFerrucci <manuferrucci@hotmail.it>
2022-11-17 22:14:09 +0100

    Controlli totali e test funzionamento

--- js/script-results.js
+++ js/script-results.js
@@ -7,9 +7,9 @@ var numeroRisultatoCorr = Number(numCorrette);
 var numeroRisultatoErr = Number(numErrate);
 var domande = Number(lunghezzaArray);
 
-var percentualeCorrette = (numeroRisultatoCorr/domande)*100;
+var percentualeCorrette = (numeroRisultatoCorr / domande) * 100;
 //console.log(percentualeCorrette + '%');
-var percentualeErrate = (numeroRisultatoErr/domande)*100;
+var percentualeErrate = (numeroRisultatoErr / domande) * 100;
 //console.log(percentualeErrate + '%');
 
 document.querySelector('#corrette').innerHTML = percentualeCorrette + '%';
@@ -18,41 +18,41 @@ document.querySelector('#contatore-corrette').innerHTML = numeroRisultatoCorr + 
 document.querySelector('#contatore-errate').innerHTML = numeroRisultatoErr + '/' + domande + ' questions';
 
 let myCanvas = document.querySelector("#myCanvas").getContext("2d");
-  let myLabels = ["WRONG", "CORRECT"];
-  let myData =  [numeroRisultatoErr, numeroRisultatoCorr];
+let myLabels = ["WRONG", "CORRECT"];
+let myData = [numeroRisultatoErr, numeroRisultatoCorr];
 
-  let chart = new Chart(myCanvas, {
-    type: "doughnut",
-    data: {
-      labels: myLabels,
-      datasets: [
-        {
-          label: "Punteggio",
-          data: myData,
-          backgroundColor: ["#D20094", "#00FFFF"],
-          borderWidth: "0.1",
-          hoverBorderColor: "#000000",
-          hoverBorderWidth: 0.1,
-          hoverOffset: 0.1,
-          cutout: "70%",
-        },
-      ],
-    },
-    options: {
-      plugins: {
-        legend: {
-          display: false,
-        },
+let chart = new Chart(myCanvas, {
+  type: "doughnut",
+  data: {
+    labels: myLabels,
+    datasets: [
+      {
+        label: "Punteggio",
+        data: myData,
+        backgroundColor: ["#D20094", "#00FFFF"],
+        borderWidth: "0.1",
+        hoverBorderColor: "#000000",
+        hoverBorderWidth: 0.1,
+        hoverOffset: 0.1,
+        cutout: "70%",
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      legend: {
+        display: false,
       },
     },
-  });
+  },
+});
 
-  var contenitoreEsito = document.querySelector('#congratulazioni');
-  var contenitoreTesto = document.querySelector('#testo-sotto');
-  if(percentualeCorrette > 60){
-    contenitoreEsito.innerHTML = 'Congratulations<br><span class="blue_text">You passed the exam.</span> ';
-    contenitoreTesto.innerHTML = "We'll send you the certificate<br> in few minutes.<br>Check your email (including<br> promotions / spam folder)";
-  }else{
-    contenitoreEsito.innerHTML = 'Non l\'hai passato? <br><span class="blue_text">Rivediti le slide</span>';
-    contenitoreTesto.innerHTML = "Sicuramente lo avete fatto <br>con<b> Michele</b>"; 
-  }
+var contenitoreEsito = document.querySelector('#congratulazioni');
+var contenitoreTesto = document.querySelector('#testo-sotto');
+if (percentualeCorrette > 60) {
+  contenitoreEsito.innerHTML = 'Congratulations<br><span class="blue_text">You passed the exam.</span> ';
+  contenitoreTesto.innerHTML = "We'll send you the certificate<br> in few minutes.<br>Check your email (including<br> promotions / spam folder)";
+} else {
+  contenitoreEsito.innerHTML = '<br><span class="blue_text">Oh no!</span>';
+  contenitoreTesto.innerHTML = "Unfortunately <b>you didn't pass it.</b>";
+}
--- js/stelle.js
+++ js/stelle.js
@@ -1,18 +1,18 @@
-  //facciamo una costante
+//facciamo una costante
 const stars = document.querySelectorAll(".stars img"); //richiamiamo, con una costante gli elementi stars che abbiamo creato nell'html
 
-    stars.forEach((star, clickedIdx) => {       //creamo un callbakc per ogni stella data dall'indice
+stars.forEach((star, clickedIdx) => {       //creamo un callbakc per ogni stella data dall'indice
     star.addEventListener("click", () => {      //aggiungiamo un evento al click   
-            
-    stars.forEach((otherStar, otherIdx) => {
-        if (otherIdx <= clickedIdx) {           //creamo un secondo index che dirà di bloccare la valutazione data, nel caso dovesse essere ricaricata la pagina 
-             otherStar.classList.add("active");
-        }
-        else{
-            otherStar.classList.remove("active");   //cosi anche se volessimo rifare la valutazione, l'index non è solo crescente ma anche decrescente
-        }
-    });
-        console.log(`star of index ${idx +1 } was clicked`);     //in uscità avremo così la stellina cliccata nell'indice
-     
+
+        stars.forEach((otherStar, otherIdx) => {
+            if (otherIdx <= clickedIdx) {           //creamo un secondo index che dirà di bloccare la valutazione data, nel caso dovesse essere ricaricata la pagina 
+                otherStar.classList.add("active");
+            }
+            else {
+                otherStar.classList.remove("active");   //cosi anche se volessimo rifare la valutazione, l'index non è solo crescente ma anche decrescente
+            }
+        });
+        console.log(`star of index ${idx + 1} was clicked`);     //in uscità avremo così la stellina cliccata nell'indice
+
     });
 })
