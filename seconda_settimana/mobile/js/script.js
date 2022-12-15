ee6034f30ed665d4d261b47f30d1bc033ebaa9f1
Rajdak <114963239+Rajdak@users.noreply.github.com>
2022-12-15 10:02:54 +0100

    home_mobile

---
+++ seconda_settimana/mobile/js/script.js
@@ -0,0 +1,10 @@
+
+async function fetchMovies() {
+    const response = await fetch(' https://striveschool-api.herokuapp.com/api/deezer/album/${albumId}')
+    .then((response) => response.json())
+    .then((datavalue) => {
+    
+})
+
+  }
+var albums = ['380094787', ]
